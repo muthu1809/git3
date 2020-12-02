5899beb3c04a452d88d28cae93fc56355741159a
muthuramalingam <muthu1809@gmail.com>
2020-12-02 14:58:02 +0530

    First in Master

---
+++ src/day1/learnings/Test.java
@@ -0,0 +1,14 @@
+package day1.learnings;
+
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//WebDriver driver = new ChromeDriver(); 
+int value = 10; 
+
+	}
+
+}
