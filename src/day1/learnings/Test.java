775f7a10e2aef2d8172d8100fb76a6c734e593bc
Muthuramalingam <muthu1809@gmail.com>
2020-12-02 15:00:36 +0530

    Update Test.java

--- src/day1/learnings/Test.java
+++ src/day1/learnings/Test.java
@@ -7,7 +7,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//WebDriver driver = new ChromeDriver(); 
-int value = 10; 
+int value = 100; 
 
 	}
 
